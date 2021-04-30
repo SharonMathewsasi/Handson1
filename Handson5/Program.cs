d2a55fb41785fe1bd5e07eb6198527f584bb4872
SharonMathewsasi <77880370+SharonMathewsasi@users.noreply.github.com>
2021-04-30 19:18:40 +0530

    Add files via upload

---
+++ Handson5/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T02HOL004
+{
+    
+    public class T02HOL004
+    {
+        static void Main(string[] args)
+        {
+        }
+        public int LeapYearCalculatorLib(int year)
+        {
+            if (year < 1753 && year > 9999)
+                return -1;
+            else
+            {
+                if (DateTime.IsLeapYear(year))
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+
+}
+© 2021 GitHub, Inc.
